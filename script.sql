a125276a2b60c60163295ba3478621a669d39ca6
Chamod Shehanka <hcsperera@outlook.com>
2019-08-01 14:49:23 +0530

    Update script.sql

--- script.sql
+++ script.sql
@@ -4,3 +4,15 @@ CREATE TABLE Branch(
     city VARCHAR(100),
     postcode VARCHAR(50)
 );
+
+CREATE TABLE Staff(
+    staffNo VARCHAR(5) PRIMARY KEY,
+    fName VARCHAR(20),
+    lName VARCHAR(20),
+    position VARCHAR(10),
+    sex CHAR(1),
+    DOB DATE,
+    salary REAL,
+    branchNo VARCHAR(5),
+    FOREIGN KEY(branchNo) REFERENCES Branch(branchNo)
+);
