3dbc4a73445aea8e68b7440295bfcebebe257bfd
Chamod Shehanka <hcsperera@outlook.com>
2019-08-01 15:04:53 +0530

    Update script.sql

--- script.sql
+++ script.sql
@@ -16,3 +16,36 @@ CREATE TABLE Staff(
     branchNo VARCHAR(5),
     FOREIGN KEY(branchNo) REFERENCES Branch(branchNo)
 );
+
+CREATE TABLE PrivateOwner(
+    ownerNo VARCHAR(5) PRIMARY KEY,
+    fName VARCHAR(20),
+    lName VARCHAR(20),
+    address VARCHAR(100),
+    tel VARCHAR(15)
+);
+
+CREATE TABLE PropertyForRent(
+    propertyNo VARCHAR(5),
+    street VARCHAR(50),
+    city VARCHAR(50),
+    postcode VARCHAR(50),
+    propertyType VARCHAR(20),
+    rooms INT,
+    rent REAL,
+    ownerNo VARCHAR(5),
+    staffNo VARCHAR(5),
+    branchNo VARCHAR(5),
+    FOREIGN KEY(ownerNo) REFERENCES PrivateOwner(ownerNo),
+    FOREIGN KEY(staffNo) REFERENCES Staff(staffNo),
+    FOREIGN KEY(branchNo) REFERENCES Branch(branchNo)
+);
+
+CREATE TABLE Client(
+    clientNo VARCHAR(5) PRIMARY KEY,
+    fName VARCHAR(20),
+    lName VARCHAR(20),
+    telNo VARCHAR(15),
+    prefType VARCHAR(10),
+    maxRent REAL
+);
